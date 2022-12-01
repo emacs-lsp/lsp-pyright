24ffd0192a4481e50ad425a78fdd54fcaf942f47
Ron Parker <rdparker@gmail.com>
2022-12-01 10:00:47 -0500

    Fix minor typo, correcting "perfer" to "prefer" (#72)

--- lsp-pyright.el
+++ lsp-pyright.el
@@ -151,7 +151,7 @@ set as `python3' to let ms-pyls use python 3 environments."
   :group 'lsp-pyright)
 
 (defcustom lsp-pyright-prefer-remote-env t
-  "If non nil, lsp-pyright will perfer remote python environment.
+  "If non nil, lsp-pyright will prefer remote python environment.
 Only available in Emacs 27 and above."
   :type 'boolean
   :group 'lsp-pyright)
